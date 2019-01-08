fcc7c24b37d2b9663db26fabd39dab5eb0e55002
Mike <mjes87@gmail.com>
2019-01-08 10:20:35 -0500

    Added some base code for BoardManager

--- TDD/TDD/BoardManager.cpp
+++ TDD/TDD/BoardManager.cpp
@@ -4,7 +4,25 @@
 
 BoardManager::BoardManager()
 {
-	
+	// Grid position following cardinal directions as per Numpad
+	int gridPos[3][3] = {
+		{7, 8, 9},
+		{4, 5, 6},
+		{1, 2, 3}
+	};
+
+	// Prints mapping out to screen
+	cout << "You are in a dungeon. It's dark. What else is there to say? \n \n";
+	cout << "\t" << pNW << "---------------" << pN << "---------------" << pNE << "\n";
+	cout << "\t|		|		|\n";
+	cout << "\t|		|		|\n";
+	cout << "\t|		|		|\n";
+	cout << "\tx---------------x---------------x\n";
+	cout << "\t|		|		|\n";
+	cout << "\t|		|		|\n";
+	cout << "\t|		|		|\n";
+	cout << "\tx---------------x---------------B\n";
+	// If playerPosition = 7 && playerA then pNW = A
 }
 
 
--- TDD/TDD/BoardManager.h
+++ TDD/TDD/BoardManager.h
@@ -6,8 +6,16 @@ using namespace std;
 class BoardManager
 {
 public:
-	int hp;
-	string playerName;
+	int gridPos[3][3];
+	int pNW;
+	int pN;
+	int pNE;
+	int pW;
+	int pC;
+	int pE;
+	int pSW;
+	int pS;
+	int pSE;
 
 	BoardManager();
 	~BoardManager();
