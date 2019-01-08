d71e3a0480f135d0822d37209e29c3cb01550426
Mike Sullivan <mjes87@gmail.com>
2019-01-08 12:11:55 -0500

    Added variables for cardinal directions

--- TDD/TDD/BoardManager.cpp
+++ TDD/TDD/BoardManager.cpp
@@ -10,6 +10,15 @@ BoardManager::BoardManager()
 		{4, 5, 6},
 		{1, 2, 3}
 	};
+	int pNW = 0;
+	int pN = 0;
+	int pNE = 0;
+	int pW = 0;
+	int pC = 0;
+	int pE = 0;
+	int pSW = 0;
+	int pS = 0;
+	int pSE = 0;
 
 	// Prints mapping out to screen
 	cout << "You are in a dungeon. It's dark. What else is there to say? \n \n";
@@ -17,11 +26,11 @@ BoardManager::BoardManager()
 	cout << "\t|		|		|\n";
 	cout << "\t|		|		|\n";
 	cout << "\t|		|		|\n";
-	cout << "\tx---------------x---------------x\n";
+	cout << "\t" << pW << "---------------" << pC << "---------------" << pE << "\n";
 	cout << "\t|		|		|\n";
 	cout << "\t|		|		|\n";
 	cout << "\t|		|		|\n";
-	cout << "\tx---------------x---------------B\n";
+	cout << "\t" << pSW << "---------------" << pS << "---------------" << pSE << "\n";
 	// If playerPosition = 7 && playerA then pNW = A
 }
 
