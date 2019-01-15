ab52106e85fb58d948e0a75c4f8767b0989f256e
Mike <mjes87@gmail.com>
2019-01-15 09:15:36 -0500

    Added playerPos variable

--- TDD/TDD/BoardManager.cpp
+++ TDD/TDD/BoardManager.cpp
@@ -10,30 +10,24 @@ BoardManager::BoardManager()
 		{4, 5, 6},
 		{1, 2, 3}
 	};
-	char pNW = 'x';
-	char pN = 'x';
-	char pNE = 'x';
-	char pW = 'x';
-	char pC = 'x';
-	char pE = 'x';
-	char pSW = 'x';
-	char pS = 'x';
-	char pSE = 'x';
+	char playerPos[9] = { 'x','x','x','x','x','x','x','x','x' };
+	char moveDir;
 
 	// Prints mapping out to screen
 	cout << "You are in a dungeon. It's dark. What else is there to say? \n \n";
-	cout << "\t" << pNW << "---------------" << pN << "---------------" << pNE << "\n";
+	cout << "\t" << playerPos[0] << "---------------" << playerPos[1] << "---------------" << playerPos[2] << "\n";
 	cout << "\t|		|		|\n";
 	cout << "\t|		|		|\n";
 	cout << "\t|		|		|\n";
-	cout << "\t" << pW << "---------------" << pC << "---------------" << pE << "\n";
+	cout << "\t" << playerPos[3] << "---------------" << playerPos[4] << "---------------" << playerPos[5] << "\n";
 	cout << "\t|		|		|\n";
 	cout << "\t|		|		|\n";
 	cout << "\t|		|		|\n";
-	cout << "\t" << pSW << "---------------" << pS << "---------------" << pSE << "\n\n";
+	cout << "\t" << playerPos[6] << "---------------" << playerPos[7] << "---------------" << playerPos[8] << "\n\n";
 	cout << "Player A				Player B\n";
 	cout << "HP:					HP: \n\n";
-	cout << "What would you like to do?\n";
+	cout << "What would you like to do?\n\n";
+	cout << "Move ";
 
 	// If playerPosition = 7 && playerA then pNW = A
 }
